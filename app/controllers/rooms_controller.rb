e169938241b1e2ee0f651e60739c620bfc8219c8
k-suke555 <keisuke.k.1991@gmail.com>
2020-09-30 14:49:39 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,4 +2,19 @@ class RoomsController < ApplicationController
   def new #チャットルーム新規作成のメソッド、form_withに渡す引数として値が空のRoomインスタンスを@roomに代入
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
