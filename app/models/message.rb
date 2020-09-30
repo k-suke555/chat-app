ea512c35ccebe8bb07eb4fc64f098857f152ba63
k-suke555 <keisuke.k.1991@gmail.com>
2020-09-30 19:02:09 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :room #1つのルームにたくさんのメッセージがある
   belongs_to :user #1人のユーザーは複数のメッセージを送信できる
+  has_one_attached :image
 
   validates :content, presence: true
 end
