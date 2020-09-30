fae6027cdd3aa38cfdcecc88534e28012bfa8301
k-suke555 <keisuke.k.1991@gmail.com>
2020-09-30 19:12:35 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,9 @@ class Message < ApplicationRecord
   belongs_to :room #1つのルームにたくさんのメッセージがある
   belongs_to :user #1人のユーザーは複数のメッセージを送信できる
   has_one_attached :image
+  validates :content, presence: true, unless: :was_attached?
 
-  validates :content, presence: true
+  def was_attached?
+    self.image.attached?
+  end
 end
